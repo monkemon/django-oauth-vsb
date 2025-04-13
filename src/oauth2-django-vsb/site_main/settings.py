dadd8f677967cb2c07210a49cb4084cb830bd8a6
monkemon <tomciobusiness@gmail.com>
2025-04-14 00:20:47 +0200

    staticfiles dirs

--- src/oauth2-django-vsb/site_main/settings.py
+++ src/oauth2-django-vsb/site_main/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [ BASE_DIR / "staticfiles" ]
 
 # for collectstatic command for deployment
 STATIC_ROOT = BASE_DIR / "staticfiles"
