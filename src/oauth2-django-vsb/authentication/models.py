52108018e7ac54d4ef2ca2651325de701823ebd4
vladcemiru <vladimir.kozak598@seznam.cz>
2025-05-03 11:22:16 +0200

    created superuser in db

--- src/oauth2-django-vsb/authentication/models.py
+++ src/oauth2-django-vsb/authentication/models.py
@@ -1,12 +1,10 @@
-from django.db import models
-
 # authentication/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
 
     USERNAME_FIELD = 'email'
